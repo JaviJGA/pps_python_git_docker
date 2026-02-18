663b1173a46787fbf0840bab8dc24cf71f129827
JaviJGA <5710397@alu.murciaeduca.es>
2026-02-18 16:38:39 +0100

    hacer que funcione el endpoint

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from bayeta import frotar
-import jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello worl"
+    return "Hola, mundo"
 
 @app.route('/frotar/<n_frotar>', methods=['GET'])
 def frotar_epicamente(n_frotar):
-    response = frotar()
+    n_frotar = int(n_frotar)
+    response = frotar(n_frotar)
     return jsonify(response)
-
 
 
 # no tocar xd
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
